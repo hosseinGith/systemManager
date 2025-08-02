2d864d6cfebcee073d584f2c7780b8a49dfca40b
hosseinGith <hosseinderees7@gmail.com>
2025-08-02 19:33:04 +0330

    fixed mail

--- components/core/utils.mjs
+++ components/core/utils.mjs
@@ -76,9 +76,9 @@ async function backupDatabaseToSQL({ database, outputFile }) {
 
 async function sendEmailWithAttachment(mailOptions, isBackUp = false) {
   const transporter = nodemailer.createTransport({
-    host: "maktababadan.ir",
-    port: 587,
-    secure: false,
+    host: "mail.maktababadan.ir",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_NAME,
       pass: process.env.EMAIL_PASS,
